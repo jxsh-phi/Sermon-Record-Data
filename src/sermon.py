cd96e79ee6ad8b97e973eeaa9f22f3edcc8c4abc
Josh-ip952 <josh.ip952@education.nsw.gov.au>
2026-09-22 17:49:15 +1000

    Create  sermon class properties

---
+++ src/sermon.py
@@ -0,0 +1,7 @@
+class Sermon():
+    def __init__(self, date, book, passage, location, speaker):
+        self.date = date
+        self.book = book
+        self.passage = passage
+        self.location = location
+        self.speaker = speaker
