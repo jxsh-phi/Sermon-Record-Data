8307e8cb7928a3e1266497a703248ced8532639e
Josh-ip952 <josh.ip952@education.nsw.gov.au>
2026-09-22 18:28:25 +1000

    Test making objects in sermon class

--- src/main.py
+++ src/main.py
@@ -1,1 +1,1 @@
-
+from sermon import *
--- src/sermon.py
+++ src/sermon.py
@@ -1,7 +1,19 @@
-class Sermon():
-    def __init__(self, date, book, passage, location, speaker):
+class SermonClass():
+    def __init__(self, date, book, passage, url, speaker):
         self.date = date
         self.book = book
         self.passage = passage
-        self.location = location
+        self.url = url
         self.speaker = speaker
+        # print(self.date, self.book, self.passage, self.url, self.speaker)
+
+    def store(self):
+        pass
+
+
+Sermon1 = SermonClass("1-3 JAN 2026", "Deuteronomy", "24:14-22",
+                      "youtu.be/pFpdztW09H4?si=L2qI-L3A3EuE119c", "李思敬博士")
+Sermon2 = SermonClass("12-13 JAN 2026", "Deuteronomy", "30:11-20",
+                      "wtccc.ca/sermons.php?select=2&sermfile=sermons-2006.dat", "李思敬博士")
+Sermon3 = SermonClass("19-20 JAN 2026", "Deuteronomy", "34:1-12",
+                      "wtccc.ca/sermons.php?select=2&sermfile=sermons-2006.dat", "李思敬博士")
